bcf453c68745f7bc90cb99cf3a71e0d7c5469a8d
caiolul <caiolucena.il@gmail.com>
2020-03-16 08:42:51 -0300

    more tests

--- pi.py
+++ pi.py
@@ -1,10 +1,11 @@
-import random
-x = random.randrange(0,1)
-"print (x)"
-def pi(c, d):
-    pi = c//d
-    return pi
-def matriz():
-    pass
+# import random
+# x = random.randrange(0,1)
+# "print (x)"
+# def pi(c, d):
+#     pi2 = c//d
+#     return pi2
+# def matriz():
+#     pass
 
+# print (pi(20,5))
 
---
+++ print.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	
+	char nome[61];
+	char _nome[61];
+
+	printf("Digite seu nome \n");
+	//fgets(nome);
+	scanf("%s",_nome);
+
+	printf("\n");
+	
+	//printf("%s",nome);
+	printf("%s \n",_nome);
+
+	printf("test");
+
+	
+	return 0;
+}
